fdc35d9f015062c399c62b34026a946450ca96c5
zisam-glitch <128255158+zisam-glitch@users.noreply.github.com>
2023-06-05 21:49:43 +0600

    commit

--- config/server.js
+++ config/server.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
+  url: env("PUBLIC_URL", "http://localhost:1337"),
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
@@ -8,3 +9,4 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
+
